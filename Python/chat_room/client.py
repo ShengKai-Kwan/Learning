981c25e394fffccc13446700001441aef5dc6491
ShengKai <skkwan97@gmail.com>
2020-05-16 21:17:43 +0800

    Updates: fix bugs in server.py and added 'Ctrl-c' termination for client.py

--- Python/chat_room/client.py
+++ Python/chat_room/client.py
@@ -1,58 +1,68 @@
 import socket
 import threading
-#from _thread import *
-import sys
 
 serverIP = input("Enter Server IP: ")
 port = int(input("Enter Port Number: "))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 name = input ("Enter a name: ")
+
+run = True
+
 def getMsg():
     try:
         while True:
-            message = server.recv (2048)
+            global run
+            if run != True:
+                break
+            message = server.recv(2048)
             print (message.decode('utf-8'))
-    except KeyboardInterrupt:
-        pass
+    except Exception as e:
+        print('Host disconnected')
+        run = False
 
 
 def sendMsg():
-    while True:
-        try:
-            message = sys.stdin.readline()
-        except Exception as e:
-            print(e)
-            pass
-        else:
-            message = name + ": " + message
-            server.send(message.encode('utf-8'))
+    try:
+        while True:
+            try:
+                message = input()
+            except EOFError:
+                print("Exiting...")
+                global run
+                run = False
+                break
+            else:
+                message = name + ": " + message
+                server.send(message.encode('utf-8'))
+    except KeyboardInterrupt:
+        print(2)
 
 
 
 try:
     server.connect ((serverIP, port))
 except Exception as e:
-    print (e)
+    print(e)
 else:
-    while True:
-        recvThread = threading.Thread(target=getMsg)
-        sendThread = threading.Thread(target=sendMsg)
+    while run:
         try:
-            recvThread.start()
-            sendThread.start ()
-        except Exception as e:
-            print(e)
+            recvThread = threading.Thread(target=getMsg)
+            sendThread = threading.Thread(target=sendMsg)
+        except:
             break
         else:
-            recvThread.join()
-            sendThread.join()
-        '''
-        for _ in range(50):
-            start_new_thread(getMsg, ())
-            start_new_thread(sendMsg, ())
-        '''
-
-    server.close()
-
-
+            recvThread.daemon = True
+            sendThread.daemon = True
+            try:
+                sendThread.start()
+                recvThread.start()
+            except:
+                pass
+            else:
+                try:
+                    sendThread.join()
+                except:
+                    pass
+                else:
+                    recvThread.join()
