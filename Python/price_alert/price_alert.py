31105c39bb1d28d84f2d3e5fb1973927b439649e
ShengKai <skkwan97@gmail.com>
2020-06-02 14:46:55 +0800

    Updates: New Features: Monitor Multiple Symbols

--- Python/price_alert/price_alert.py
+++ Python/price_alert/price_alert.py
@@ -5,19 +5,25 @@ import time
 import os
 
 """
-This application purposely for price monitoring and alerting. with using the Rakuten trade platform (Malaysia) to collect real-time data and Whatsapp for alerting the user.
+This application purposely for price monitoring and alerting. 
+with using the Rakuten trade platform (Malaysia) to collect real-time data 
+and Whatsapp for alerting the user.
 
 """
 
 url = "https://www.rakutentrade.my/login/"
-usrname = ""                # set your rakuten username
-psswd = ""                  # set your rakuten password
-symbol = ""                 # set the symbol you wish to monitor
-maxTpAlert = 0.17           # when real-time price larger or equal maxTpAlert, the application will trigger an alert
-minTpAlert = 0.16           # when real-time price smaller or equal minTpAlert, the application will trigger an alert
-refreshTime = 60            # The application will collect data every {refreshTime} seconds
-load_time = 6               # Loading time, to wait the browser load the page. Increase the load_time if you have poor/slow internet connection, else decrease
-whatsapTarget = ""          # The target name. Replace with your friend or group name. For myself, i created a new group to send the alert message.
+usrname = ""                    # set your rakuten username
+psswd = ""                     # set your rakuten password
+#symbol = "istone"                      # set the symbol you wish to monitor
+#maxTpAlert = 0.17                      # when real-time price larger or equal maxTpAlert, the application will trigger an alert
+#minTpAlert = 0.16                      # when real-time price smaller or equal minTpAlert, the application will trigger an alert
+symbols = [                             # [[{symbol}, {minTpAlert}, {maxTpAlert}, 0], [{symbol2}, {minTpAlert2}, {maxTpAlert2}, 0], []...]
+    ['istone', 0.16, 0.17, 0],
+    ['binacom', 0.43, 0.455, 0]
+]
+refreshTime = 60                        # The application will collect data every {refreshTime} seconds
+load_time = 6                           # Loading time, to wait the browser load the page. Increase the load_time if you have poor/slow internet connection, else decrease
+whatsapTarget = ""               # The target name. Replace with your friend or group name. For myself, i created a new group to send the alert message.
 
 
 chrome_opt = webdriver.ChromeOptions()
@@ -43,7 +49,69 @@ def alert(msg, whatsappTab):
         inputField.send_keys(msgs[i])
 
     driver.find_element_by_css_selector ("button._35EW6").click()
-    driver.switch_to.window ("rakutenTab")
+
+def searchSymbol(whatsappTab):
+
+    for symbol in symbols:
+        print("Opening new Tab for: "+ symbol[0] + "...")
+        driver.execute_script("window.open('about:blank', '" + symbol[0] + "');")
+        driver.switch_to.window(symbol[0])
+        driver.get (url)
+        # search for the symbol
+        time.sleep(load_time+2)
+        print("Entering Symbol: " + symbol[0] + "...")
+        driver.find_element_by_css_selector("input.search-input.scene-input.ui-input-text.ui-body-a").send_keys(symbol[0])
+        time.sleep (load_time)
+        print("Searching Symbol: " + symbol[0] + "...")
+        driver.find_element_by_css_selector("ul.symbol_list_ul").click()
+
+
+        # Inform user : Initiating Monitoring on {symbol}
+        time.sleep(load_time+1)
+        print('Monitoring :' + symbol[0].upper())
+        try:
+            alert("Monitoring Symbol: " + symbol[0].upper(), whatsappTab)
+        except Exception as e:
+            print(e)
+            print("Check Whatsapp Connection!")
+
+def monitorSymbol(whatsappTab):
+    driver.switch_to.window (symbols[0][0])
+    driver.find_element_by_css_selector ("div.refresh.date-and-time").click ()
+    dtime = driver.find_element_by_css_selector ("div.time-date-val").text[14:]
+    log = dtime.ljust (15, " ")
+
+    for symbol in symbols:
+        # monitor
+        time.sleep (load_time)
+        driver.switch_to.window (symbol[0])
+        driver.find_element_by_css_selector ("div.refresh.date-and-time").click ()
+
+        price = driver.find_element_by_css_selector ("span.last").text
+        dtime = driver.find_element_by_css_selector ("div.time-date-val").text[14:]
+        volume = driver.find_element_by_css_selector ("div.info-blk.vol > div.val").text
+        volume_string_len = len (volume) - 1
+        if volume[volume_string_len] == "M":
+            volume = int (float (volume[0:volume_string_len - 1]) * 1000000)
+        else:
+            volume = int (volume.replace (",", ""))
+        volume = int (volume)
+        volume_dif = volume - symbol[3] # symbol[3] is last volume recorded, volume difference = current total volume - last recorded volume
+        symbol[3] = volume # update latest volume
+        log = log + "|" + str(price).ljust(10, ' ') + "|" + str(format(int(volume), ',')).ljust(10, ' ')
+        if float (price) >= symbol[2]:
+            alert (
+                "{symbol};Max Target Price: {maxTp};Total Volume: {volume};Current Price: {price};Vol. Chg.: {volume_dif};{dtime}".format (
+                    symbol=symbol[0].upper (), maxTp=symbol[2], volume=format (volume, ','), price=price,
+                    volume_dif=format (volume_dif, ','), dtime=dtime), whatsappTab)
+
+        if float (price) <= symbol[1]:
+            alert (
+                "{symbol};Min Target Price: {minTp};Total Volume: {volume};Current Price: {price};Vol. Chg.: {volume_dif};{dtime}".format (
+                    symbol=symbol[0].upper (), minTp=symbol[1], volume=format (volume, ','), price=price,
+                    volume_dif=format (volume_dif, ','), dtime=dtime), whatsappTab)
+
+    print(log + "|")
 
 def main():
     global last_volume
@@ -68,55 +136,21 @@ def main():
     print("Logging in...")
     driver.find_element_by_id ('login-btn').click()
 
+    searchSymbol (whatsappTab)
+    os.system('CLS')
 
-    # search for the symbol
-    time.sleep(load_time+2)
-    print("Entering Symbol...")
-    driver.find_element_by_css_selector("input.search-input.scene-input.ui-input-text.ui-body-a").send_keys(symbol)
-    time.sleep (load_time)
-    print("Searching Symbol...")
-    driver.find_element_by_css_selector("ul.symbol_list_ul").click()
+    header = ' '.center(15, ' ')
+    header2 = 'Time'.center(15, ' ')
+    for symbol in symbols:
+        header = header + '|' + symbol[0].center(21)
+        header2 = header2 + "|" + "Price".center(10, " ")+ "|" + "Vol. Chg.".center(10, " ")
 
+    print(header + "|")
+    print(header2 + "|")
 
-    # get latest details of the symbol
-    time.sleep(load_time+1)
-    try:
-        os.system('CLS')
-        print("Initiating Monitoring... ")
-        alert("Monitoring Symbol: " + symbol.upper(), whatsappTab)
-    except Exception as e:
-        print("Check Whatsapp Connection!")
-
-    time.sleep (load_time)
-    last_volume = 0             # initial volume always 0
     while True:
-        price = driver.find_element_by_css_selector ("span.last").text
-        dtime = driver.find_element_by_css_selector ("div.time-date-val").text
-        volume = driver.find_element_by_css_selector ("div.info-blk.vol > div.val").text
-        volume_string_len = len(volume)-1
-        if volume[volume_string_len] == "M":
-            volume = int(float(volume[0:volume_string_len - 1]) * 1000000)
-        else:
-            volume = int(volume.replace(",", ""))
-        volume = int(volume)
-        volume_dif = volume - last_volume
-        last_volume = volume
-        print (dtime + ', Total Volume: ' + format(volume, ",") + ', RM: ' + price + ', Volume changes: ' + format(volume_dif, ","))
-
-        if float(price) >= maxTpAlert:
-            alert (
-                "{symbol};Max Target Price: {maxTp};Total Volume: {volume};Current Price: {price};Volume: {volume_dif};{dtime}".format (
-                    symbol=symbol.upper (), maxTp=maxTpAlert, volume=format (volume, ','), price=price,
-                    volume_dif=format (volume_dif, ','), dtime=dtime), whatsappTab)
-
-        if float(price) <= minTpAlert:
-            alert (
-                "{symbol};Min Target Price: {minTp};Total Volume: {volume};Current Price: {price};Volume: {volume_dif};{dtime}".format (
-                    symbol=symbol.upper (), minTp=minTpAlert, volume=format (volume, ','), price=price,
-                    volume_dif=format (volume_dif, ','), dtime=dtime), whatsappTab)
-
+        monitorSymbol(whatsappTab)
         time.sleep (refreshTime)  # stop for {refreshTime}} seconds
-        driver.find_element_by_css_selector ("div.refresh.date-and-time").click()
 
 try:
     main()
